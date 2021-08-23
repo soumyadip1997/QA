55e27cccee503fe466fddc972b944823c388937f
soumyadip1997 <40233488+soumyadip1997@users.noreply.github.com>
2021-08-22 22:08:35 -0600

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ def train(GNN,dataloader):
     loss=nn.MSELoss()
     loss_list=[]
     
-    for epoch in range(5):
+    for epoch in range(2):
         loss_per_epoch=0
         print(epoch)
         for i,batch in enumerate(dataloader):
